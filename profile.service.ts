eecfbbfc64fd0dfc22ebf99d03bac2cb672a780d
chrikin1 <chrisianbartoo@gmail.com>
2018-08-17 14:42:13 +0300

    started project

---
+++ profile.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+export class ProfileService {
+
+@Injectable()
+export class ProfileService {
+  constructor() { }
+}
