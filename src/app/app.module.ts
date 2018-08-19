1a6f4596e787e2c815a6adbdb3a5f58526a51c15
chrikin1 <chrisianbartoo@gmail.com>
2018-08-19 22:51:19 +0300

    changes to ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { ApiService } from './api-http/api.service';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { LinkComponent } from './link/link.component';
 
 @NgModule({
   declarations: [
