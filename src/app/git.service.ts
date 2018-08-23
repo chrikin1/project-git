01480700de333c2206aae1ab6cba8f092220358e
chrikin1 <chrisianbartoo@gmail.com>
2018-08-23 16:17:56 +0300

    logic dealt

--- src/app/git.service.ts
+++ src/app/git.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import {Git} from  './git';
 import {HttpClient} from '@angular/common/http';
-@Injectable({
+import {environment} from  '../environments/environment'
+@Injectable({ 
   providedIn: 'root'
 })
 export class GitService {
 git:Git;
+constructor(private http: HttpClient) {}
 getgit(f) {
   interface ApiResponse {
 name: string;
@@ -13,10 +15,10 @@ login: string;
 avatar_url: any;
   }
 const promise = new Promise ((resolve, reject) => {
-  this.http.get <ApiResponse> ().toPromise().then(reducted => {
-    this.git.name = reducted.name;
-    this.git.login =reducted.login;
-    this.git.avatar_url = reducted.avatar_url;
+  this.http.get <ApiResponse> (environment.url+environment.token).toPromise().then(res => {
+    this.git.name = res.name;
+    this.git.login =res.login;
+    this.git.avatar_url = res.avatar_url;
     resolve();
   },    error => {
     this.git.name = 'reducted.name';
@@ -32,5 +34,5 @@ return promise;
 
 Collapse 
 
-  constructor() { }
+  
 }
